8165662554fddb6edcbc0fcb105efbaea4f30f79
Alex Ovsiychuk <b13u13@gmail.com>
2016-12-01 00:47:59 +0200

    Cart destroy and line item destroy

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,6 +3,20 @@ class Cart < ApplicationRecord
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
   private
 
   def ensure_not_referenced_by_any_line_item
--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,7 +17,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, product_id: products(:ruby).id
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     assert_redirected_to cart_path(assigns(:line_item).cart)
@@ -34,7 +34,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: { line_item: {product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
